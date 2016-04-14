4eaa6e1cd6f29dba8515b9806d1a1a97868a3ade
Giles Miclotte <giles.miclotte@intec.ugent.be>
2016-04-15 01:29:28 +0200

    Correctly process genome files with multiple sequences

--- src/bionanosim.py
+++ src/bionanosim.py
@@ -4,6 +4,37 @@ from util import fasta_parse as RF
 from knick import knicks
 from bnx import bnx_header, bnx_entry
 from noise import knick_molecule, strand
+import random
+
+def process_sequence(meta, seq, circular, patterns):
+        max_pattern_len = max([len(p) for p in patterns])
+        seqLen = len(seq)
+        if circular:
+                seq += seq[0:max_pattern_len]
+        fk, rck = knicks(seq, patterns)
+        if circular:
+                #remove duplicate hits
+                while fk[-1][0] >= seqLen:
+                        fk.pop()
+                while rck[-1][0] >= seqLen:
+                        rck.pop()
+        return fk, rck
+
+def generate_molecules(seqLens, fks, rcks, coverage, avg, fprate, fnrate, sd, circular, min_mol_len):
+        size = 0
+        seqCount = len(seqLens)
+        cumSeqLens = [sum(seqLens[:k + 1]) for k in range(seqCount)]
+        while size < coverage * cumSeqLens[-1]:
+                idx = seqCount
+                r = random.random() * cumSeqLens[-1]
+                while 0 < idx and r < cumSeqLens[idx - 1]:
+                        idx -= 1
+                i, m = knick_molecule([fks[idx], rcks[idx]][strand()], seqLens[idx], avg, fprate, fnrate, sd, circular[idx])
+                l = i[0]
+                if l >= min_mol_len:
+                        size += l
+                        yield l, m
+
 
 ifname = sys.argv[1]
 patterns = [sys.argv[2]]
@@ -21,28 +52,26 @@ fprate = 1.0 #number of fp in 100kb
 fnrate = 0.15 #fn rate of true labels
 sd = 1500 #sd of knick position
 
-max_pattern_len = max([len(p) for p in patterns])
+
 moleculeID = 0
+fks = []
+rcks = []
+seqLens = []
 for meta, seq in RF(ifname):
-        seqLen = len(seq)
-        if circular:
-                seq += seq[0:max_pattern_len]
-        fk, rck = knicks(seq, patterns)
+        print('Indexing sequence: ' + meta)
+        seqLens.append(len(seq))
+        fk, rck = process_sequence(meta, seq, circular, patterns)
+        fks.append(fk)
+        rcks.append(rck)
+        meta = None
         seq = None
-        if circular:
-                #remove duplicate hits
-                while fk[-1][0] >= seqLen:
-                        fk.pop()
-                while rck[-1][0] >= seqLen:
-                        rck.pop()
-        size = 0
-        while size < coverage * seqLen:
-                i, m = knick_molecule([fk, rck][strand()], seqLen, avg, fprate, fnrate, sd, circular)
-                l = i[0]
-                if l >= min_mol_len:
-                        size += l
-                        moleculeID += 1
-                        bnx_entry((moleculeID, l), m, ofile)
+
+print('Generating reads...')
+for l, m in generate_molecules(seqLens, fks, rcks, coverage, avg, fprate, fnrate, sd, [circular] * len(fks), min_mol_len):
+                moleculeID += 1
+                bnx_entry((moleculeID, l), m, ofile)
+print('Finished.')
+
 
 '''
 import sys
