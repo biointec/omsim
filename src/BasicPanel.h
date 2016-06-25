7a1a3e06cf2e647294b8a1ba79d64fa507c51942
Giles Miclotte <giles.miclotte@intec.ugent.be>
2016-06-25 13:21:16 +0200

    Sketch Basic Layout

--- src/BasicPanel.h
+++ src/BasicPanel.h
@@ -2,6 +2,7 @@
 #define BASICPANEL_HPP
 
 #include "wx.hpp"
+#include <wx/stattext.h>
 
 class BasicPanel : public wxPanel
 {
@@ -9,20 +10,60 @@ class BasicPanel : public wxPanel
                 BasicPanel(wxWindow *parent, wxWindowID id)
                       : wxPanel(parent, id)
                 {
-                        wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
-                        wxBoxSizer *hbox1 = new wxBoxSizer(wxHORIZONTAL);
-                        wxBoxSizer *hbox2 = new wxBoxSizer(wxHORIZONTAL);
+                        wxBoxSizer *mainbox = new wxBoxSizer(wxVERTICAL);
                         
+                        /*
+                                xml box
+                        */
+                        wxBoxSizer *xmlbox = new wxBoxSizer(wxVERTICAL);
+                        wxStaticText *xmlTitle = new wxStaticText(this, wxID_ANY, wxT("Configuration file"));
+                        wxButton *xmlButton = new wxButton(this, -1, wxT("Open configuration file"));
+                        xmlbox->Add(xmlTitle);
+                        xmlbox->Add(xmlButton);
+                        
+                        /*
+                                fasta box
+                        */
+                        wxBoxSizer *fastabox = new wxBoxSizer(wxVERTICAL);
+                        wxStaticText *fastaTitle = new wxStaticText(this, wxID_ANY, wxT("Genome files"));
                         wxButton *ok = new wxButton(this, -1, wxT("Ok"));
-                        hbox1->Add(ok);
+                        fastabox->Add(fastaTitle);
+                        fastabox->Add(ok);
                         
+                        /*
+                                enzyme box
+                        */
+                        wxBoxSizer *enzymebox = new wxBoxSizer(wxVERTICAL);
+                        wxStaticText *enzymeTitle = new wxStaticText(this, wxID_ANY, wxT("Enzymes"));
                         wxButton *cancel = new wxButton(this, -1, wxT("Cancel"));
-                        hbox2->Add(cancel);
+                        enzymebox->Add(enzymeTitle);
+                        enzymebox->Add(cancel);
                         
-                        vbox->Add(hbox1);
-                        vbox->Add(hbox2);
+                        /*
+                                length box
+                        */
+                        wxBoxSizer *lengthbox = new wxBoxSizer(wxVERTICAL);
+                        wxStaticText *lengthTitle = new wxStaticText(this, wxID_ANY, wxT("Size distribution"));
+                        wxBoxSizer *sizedistbox = new wxBoxSizer(wxHORIZONTAL);
+                        wxStaticText *meanText = new wxStaticText(this, wxID_ANY, wxT("Mean:"));
+                        wxTextCtrl *mean = new wxTextCtrl(this, -1, wxT(""), wxPoint(-1, -1), wxSize(-1, -1));
+                        wxStaticText *stdText = new wxStaticText(this, wxID_ANY, wxT("Standard deviation:"));
+                        wxTextCtrl *std =  new wxTextCtrl(this, -1, wxT(""), wxPoint(-1, -1), wxSize(-1, -1));
+                        sizedistbox->Add(meanText);
+                        sizedistbox->Add(mean);
+                        sizedistbox->Add(stdText);
+                        sizedistbox->Add(std);
+                        lengthbox->Add(lengthTitle);
+                        lengthbox->Add(sizedistbox);
                         
-                        SetSizer(vbox);
+                        
+                        
+                        mainbox->Add(xmlbox);
+                        mainbox->Add(fastabox);
+                        mainbox->Add(enzymebox);
+                        mainbox->Add(lengthbox);
+                        
+                        SetSizer(mainbox);
                 }
 };
 
