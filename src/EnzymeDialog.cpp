079f2c9bc7d0dba3e2f19103cbf9a0f3689242e7
Giles Miclotte <giles.miclotte@intec.ugent.be>
2016-06-29 22:34:36 +0200

    Change EnzymeDialog layout

--- src/EnzymeDialog.cpp
+++ src/EnzymeDialog.cpp
@@ -7,59 +7,52 @@ EnzymeDialog::EnzymeDialog(wxWindow *parent, wxWindowID id, const wxString &titl
       : wxDialog(parent, id, title, pos, size, style, name), e(e_)
 {
         wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
-        wxBoxSizer *hbox1 = new wxBoxSizer(wxHORIZONTAL);
-        wxBoxSizer *hbox2 = new wxBoxSizer(wxHORIZONTAL);
-        wxBoxSizer *hbox3 = new wxBoxSizer(wxHORIZONTAL);
-        wxBoxSizer *hbox4 = new wxBoxSizer(wxHORIZONTAL);
-        wxBoxSizer *hbox5 = new wxBoxSizer(wxHORIZONTAL);
-        wxBoxSizer *hbox6 = new wxBoxSizer(wxHORIZONTAL);
+        wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
+        
+        wxFlexGridSizer *grid = new wxFlexGridSizer(2, 10, 10);
         
         wxStaticText *idText = new wxStaticText(this, wxID_ANY, wxT("Enzyme ID:"));
         idCtrl = new wxTextCtrl(this, wxID_EnzymeID, e.id, wxPoint(-1, -1), wxSize(-1, -1));
-        hbox1->Add(idText, 0, wxEXPAND | wxTOP | wxLEFT, 20);
-        hbox1->Add(idCtrl, 0, wxEXPAND | wxTOP | wxRIGHT, 20);
+        grid->Add(idText);
+        grid->Add(idCtrl);
         Connect(wxID_EnzymeID, wxEVT_TEXT, 
                 wxCommandEventHandler(EnzymeDialog::OnTextID));
         
         wxStaticText *patternText = new wxStaticText(this, wxID_ANY, wxT("Pattern:"));
         patternCtrl = new wxTextCtrl(this, wxID_EnzymePattern, e.pattern, wxPoint(-1, -1), wxSize(-1, -1));
-        hbox2->Add(patternText, 0, wxEXPAND | wxLEFT, 20);
-        hbox2->Add(patternCtrl, 0, wxEXPAND | wxRIGHT, 20);
+        grid->Add(patternText);
+        grid->Add(patternCtrl);
         Connect(wxID_EnzymePattern, wxEVT_TEXT, 
                 wxCommandEventHandler(EnzymeDialog::OnTextPattern));
         
         wxStaticText *fnText = new wxStaticText(this, wxID_ANY, wxT("False negative rate (per nick):"));
         fnCtrl = new wxTextCtrl(this, wxID_EnzymeFN, e.fn, wxPoint(-1, -1), wxSize(-1, -1));
-        hbox3->Add(fnText, 0, wxEXPAND | wxLEFT, 20);
-        hbox3->Add(fnCtrl, 0, wxEXPAND | wxRIGHT, 20);
+        grid->Add(fnText);
+        grid->Add(fnCtrl);
         Connect(wxID_EnzymeFN, wxEVT_TEXT, 
                 wxCommandEventHandler(EnzymeDialog::OnTextFN));
         
         wxStaticText *fpText = new wxStaticText(this, wxID_ANY, wxT("False positive rate (per 100 Kb):"));
         fpCtrl = new wxTextCtrl(this, wxID_EnzymeFP, e.fp, wxPoint(-1, -1), wxSize(-1, -1));
-        hbox4->Add(fpText, 0, wxEXPAND | wxLEFT, 20);
-        hbox4->Add(fpCtrl, 0, wxEXPAND | wxRIGHT, 20);
+        grid->Add(fpText);
+        grid->Add(fpCtrl);
         Connect(wxID_EnzymeFP, wxEVT_TEXT, 
                 wxCommandEventHandler(EnzymeDialog::OnTextFP));
         
         wxStaticText *labelText = new wxStaticText(this, wxID_ANY, wxT("Label:"));
         labelCtrl = new wxTextCtrl(this, wxID_EnzymeLabel, e.label, wxPoint(-1, -1), wxSize(-1, -1));
-        hbox5->Add(labelText, 0, wxEXPAND | wxBOTTOM | wxLEFT, 20);
-        hbox5->Add(labelCtrl, 0, wxEXPAND | wxBOTTOM | wxRIGHT, 20);
+        grid->Add(labelText);
+        grid->Add(labelCtrl);
         Connect(wxID_EnzymeLabel, wxEVT_TEXT, 
                 wxCommandEventHandler(EnzymeDialog::OnTextLabel));
         
         wxButton *ok = new wxButton(this, wxID_OK, wxT("Ok"));
         wxButton *cancel = new wxButton(this, wxID_CANCEL, wxT("Cancel"));
-        hbox6->Add(ok, 0, wxEXPAND | wxBOTTOM | wxLEFT, 20);
-        hbox6->Add(cancel, 0, wxEXPAND | wxBOTTOM | wxLEFT, 20);
+        hbox->Add(ok, 0, wxEXPAND | wxALL, 20);
+        hbox->Add(cancel, 0, wxEXPAND | wxALL, 20);
         
-        vbox->Add(hbox1);
-        vbox->Add(hbox2);
-        vbox->Add(hbox3);
-        vbox->Add(hbox4);
-        vbox->Add(hbox5);
-        vbox->Add(hbox6);
+        vbox->Add(grid, 0, wxEXPAND | wxALL, 20);
+        vbox->Add(hbox);
         
         SetSizerAndFit(vbox);
 }
