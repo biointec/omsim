24d04ce70d47c3076db3e11ce40e01dbacf31273
Giles Miclotte <giles.miclotte@intec.ugent.be>
2016-06-30 14:22:37 +0200

    Move the source files to src/

--- compile.sh
+++ compile.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-mkdir -p bin
+mkdir -p ../bin
 
 #mkdir -p debug
 #cd debug
 #cmake -DCMAKE_BUILD_TYPE=DEBUG ..
 #make
-#cp src/omsimgui/omsimgui ../bin/omsimgui
+#cp src/omsimgui ../../bin/omsimgui
 #cd ..
 
 mkdir -p release
 cd release
 cmake ..
 make
-cp src/omsimgui/omsimgui ../bin/omsimgui
+cp src/omsimgui ../../bin/omsimgui
 cd ..
 
