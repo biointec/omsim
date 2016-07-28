d3f7ecd3f42ecbc9493c0df98e94e16e1157e110
Giles Miclotte <giles.miclotte@intec.ugent.be>
2016-07-28 10:49:46 +0200

    Add proper descriptions

--- omsimgui/src/configuration.hpp
+++ omsimgui/src/configuration.hpp
@@ -53,44 +53,52 @@ struct configuration {
         configuration(){
                 circular = false;
                 
-                set("name", "Unnamed", "name");
-                set("prefix", "omsim_output", "prefix");
-                set("byte_prefix", "omsim", "byte_prefix");
-                set("coverage", "0", "coverage");
-                set("chips", "1", "chips");
-                set("scans_per_chip", "30", "scans_per_chip");
-                set("scan_size", "1500", "scan_size");
-                set("avg_mol_len", "90000", "avg_mol_len");
-                set("sd_mol_len", "85000", "sd_mol_len");
-                set("min_mol_len", "1", "min_mol_len");
-                set("max_mol_len", "2500000", "max_mol_len");
-                set("min_nicks", "1", "min_nicks");
-                set("label_snr_filter_type", "Static", "label_snr_filter_type");
-                set("min_label_SNR", "0.0", "min_label_SNR");
-                set("nick_sd", "50", "nick_sd");
-                set("fragile_same", "50", "fragile_same");
-                set("fragile_opposite", "150", "fragile_opposite");
-                set("fragile_treshold", "25", "fragile_treshold");
-                set("fragile_factor", "3", "fragile_factor");
-                set("label_mu", "1500", "label_mu");
-                set("label_treshold", "500", "label_treshold");
-                set("label_factor", "100", "label_factor");
-                set("chimera_rate", "0.01", "chimera_rate");
-                set("chimera_mu", "1500", "chimera_mu");
-                set("chimera_sigma", "500", "chimera_sigma");
-                set("stretch_factor", ".85", "stretch_factor");
-                set("stretch_chip_sd", "0.01", "stretch_chip_sd");
-                set("stretch_scan_sd", "0.001", "stretch_scan_sd");
-                set("molecule_AI_mu", "0.3", "molecule_AI_mu");
-                set("molecule_AI_sd", "0.2", "molecule_AI_sd");
-                set("label_AI_mu", "0.07", "label_AI_mu");
-                set("label_AI_sd", "0.04", "label_AI_sd");
-                set("molecule_SNR_mu", "15.0", "molecule_SNR_mu");
-                set("molecule_SNR_sd", "75.0", "molecule_SNR_sd");
-                set("label_SNR_mu", "14.0", "label_SNR_mu");
-                set("label_SNR_sd", "11.0", "label_SNR_sd");
-                set("sim_batch_size", "100000", "sim_batch_size");
-                set("seed", "", "seed");
+                set("name", "Unnamed", "Name");
+                set("prefix", "omsim_output", "Prefix");
+                set("byte_prefix", "omsim", "Byte Prefix");
+
+                set("coverage", "0", "Minimal Coverage");
+                set("chips", "1", "Chip Count");
+                set("scans_per_chip", "30", "Scans Per Chip");
+                set("scan_size", "1500", "Scan Size (Megabase)");
+
+                set("avg_mol_len", "90000", "Molecule Length: Mean");
+                set("sd_mol_len", "85000", "Molecule Length: Standard Deviation");
+                set("min_mol_len", "1", "Molecule Length: Minimum");
+                set("max_mol_len", "2500000", "Molecule Length: Maximum");
+                set("min_nicks", "1", "Minimal Number of Nicks");
+
+                set("label_mu", "1500", "Label Merge: Mean");
+                set("label_treshold", "500", "Label Merge: Treshold");
+                set("label_factor", "100", "Label Merge: Factor");
+                set("nick_sd", "50", "Label position: Standard Deviation");
+
+                set("fragile_same", "50", "Fragile Distance: Same Strand Mean");
+                set("fragile_opposite", "150", "Fragile Distance: Opposite Strand Mean");
+                set("fragile_treshold", "25", "Fragile Distance: Treshold");
+                set("fragile_factor", "3", "Fragile Distance: Factor");
+
+                set("chimera_rate", "0.01", "Chimera Rate");
+                set("chimera_mu", "1500", "Chimera Distance: Mean");
+                set("chimera_sigma", "500", "Chimera Distance: Standard Deviation");
+                
+                set("stretch_factor", ".85", "Stretch Factor: Mean");
+                set("stretch_chip_sd", "0.01", "Stretch Factor: Chip Standard Deviation");
+                set("stretch_scan_sd", "0.001", "Stretch Factor: Scan Standard Deviation");
+                
+                set("molecule_AI_mu", "0.3", "Molecule AI: Mean");
+                set("molecule_AI_sd", "0.2", "Molecule AI: Standard Deviation");
+                set("label_AI_mu", "0.07", "Label AI: Mean");
+                set("label_AI_sd", "0.04", "Label AI: Standard Deviation");
+                set("molecule_SNR_mu", "15.0", "Molecule SNR: Mean");
+                set("molecule_SNR_sd", "75.0", "Molecule SNR: Standard Deviation");
+                set("label_snr_filter_type", "Static", "Label SNR: Filter Type");
+                set("min_label_SNR", "0.0", "Label SNR: Minimum");
+                set("label_SNR_mu", "14.0", "Label SNR: Mean");
+                set("label_SNR_sd", "11.0", "Label SNR: Standard Deviation");
+
+                set("sim_batch_size", "100000", "Simulation Batch Size");
+                set("seed", "", "PRNG Seed");
         };
 };
 
