3437f2acf8230731d83bc860d6487cd95cf670dc
Giles Miclotte <Giles.Miclotte@GMail.com>
2023-10-19 15:22:27 +0200

    v1.2.0 Paths are now relative to the working directory  instead of the simulation XML

--- omsim/src/omsim/omsim.py
+++ omsim/src/omsim/omsim.py
@@ -307,7 +307,7 @@ def print_usage():
 
 
 def main(argv=None):
-        version = "v1.1.10"
+        version = "v1.2.0"
         print_welcome(version)
         if argv is None:
                 argv = sys.argv
