46d38cb3adb1d59e234ff28777e0d2d8bee3c03f
Giles Miclotte <Giles.Miclotte@GMail.com>
2023-10-26 18:22:35 +0200

    Add examples for molecule count settings

---
+++ example/Molecules-Count/run.sh
@@ -0,0 +1,5 @@
+python ../../omsim/src/omsim/__main__.py example-chips.xml
+python ../../omsim/src/omsim/__main__.py example-coverage.xml
+python ../../omsim/src/omsim/__main__.py example-min_num_mols.xml
+python ../../omsim/src/omsim/__main__.py example-max_num_mols.xml
+python ../../omsim/src/omsim/__main__.py example-num_mols.xml
